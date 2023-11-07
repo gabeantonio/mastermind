04832eb7923c9f31c660cadc5d5afcc9a09f748c
gabeantonio <antoniogabe99@gmail.com>
2023-11-07 12:22:13 -0800

    Initialize Mastermind class and import to init script

--- init_mastermind.py
+++ init_mastermind.py
@@ -1,7 +1,10 @@
+from mastermind import Mastermind
+
 class Play:
     def main():
         print('Hello World!')
-
+        mastermind = Mastermind()
+        print(mastermind)
     
     if __name__ == '__main__':
         main()
