190fa037d6b5ed72993c99a7d82844138086b4d5
gabeantonio <antoniogabe99@gmail.com>
2023-11-07 12:36:45 -0800

    Add set variables for attempts allowed and combo length

--- mastermind.py
+++ mastermind.py
@@ -1,4 +1,6 @@
 class Mastermind:
+    TOTAL_TRIES = 10
+    COMBO_LENGTH = 4
     def __init__(self, hidden_combination: str):
         self.hidden_combination = hidden_combination
         self.guesses = []
