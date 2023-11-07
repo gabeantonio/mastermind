c5f931e9e468cc4a4b2c66b0ee626aa1d077b871
gabeantonio <antoniogabe99@gmail.com>
2023-11-07 12:57:09 -0800

    Initialzie continue guess logic in init_mastermind.py script

--- init_mastermind.py
+++ init_mastermind.py
@@ -3,8 +3,9 @@ from mastermind import Mastermind
 class Play:
     def main():
         print('Hello World!')
-        mastermind = Mastermind()
-        print(mastermind)
+        mastermind = Mastermind('')
+        while mastermind.continue_guessing:
+            pass
 
     if __name__ == '__main__':
         main()
