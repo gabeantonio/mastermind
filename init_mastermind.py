b853afea45d282523bb3a1ab23ff8fbf1cfe27b3
gabeantonio <antoniogabe99@gmail.com>
2023-11-07 22:34:29 -0800

    Add API call to get random combination

--- init_mastermind.py
+++ init_mastermind.py
@@ -1,8 +1,21 @@
 from mastermind import Mastermind
+import requests
 
 class Play:
     def main():
-        print('Hello World!')
+        # Make a call to the API:
+        api_url = "https://www.random.org/integers/?num=4&min=1&max=7&col=1&base=10&format=plain&rnd=new"
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            combination = ''
+            combination += response.text
+            # For some reason the text comes back in rows, so the below operation removes the spaces from the row:
+            hidden_combination = combination.replace('\n', '')
+        else:
+            print(f'Failed to get a random combination. Status code: {response.status_code}')
+        print('HIDDEN COMBINATION -->', hidden_combination)
+
+
         mastermind = Mastermind('1234')
         while mastermind.continue_guessing:
             user_guess = input('Type your guess here: ')
