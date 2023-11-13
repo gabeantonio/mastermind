a7245690ca5457cb153ae4968cbc27381c65d5cb
gabeantonio <antoniogabe99@gmail.com>
2023-11-12 22:38:43 -0800

    Re-include pseudocode

--- init_mastermind.py
+++ init_mastermind.py
@@ -3,33 +3,52 @@ import requests
 from colorama import Fore
 
 def main():
+    # MAKE API CALL AND STORE RETURNED COMBINATION IN THE BELOW VARIABLE:
     hidden_combination = get_random_combination()
+    # FOR TESTING:
+    print('HIDDEN COMBINATION: ', hidden_combination)
+    # INITIALIZE THE PLAYER SCORE TO 0:
     player_score = 0
+    # CALL THE PLAY GAME FUNCTION:
     play_game(hidden_combination, player_score)
 
 def play_game(hidden_combination: str, player_score: int):
+    # INSTANTIATE THE MASTERMIND CLASS:
     mastermind = Mastermind(hidden_combination)
+    # INITIALIZE A WHILE LOOP TO CONTINUE LOOPING UNTIL THE MASTERMIND.CONTINUE_GUESSING FUNCTION IS STILL TRUE:
     while mastermind.continue_guessing():
+        # GET THE USER'S GUESS:
         user_guess = input('Type your guess here: ')
+        # WRITE LOGIC FOR USER INPUT VALIDATION:
         if len(user_guess) < mastermind.COMBO_LENGTH:
             print(Fore.RED + f'Input too short. Please make sure your input is {mastermind.COMBO_LENGTH} digits long. \n' + Fore.RESET)
             continue
         elif len(user_guess) > mastermind.COMBO_LENGTH:
             print(Fore.RED + f'Input too long. Please make sure your input is only {mastermind.COMBO_LENGTH} long. \n' + Fore.RESET)
             continue
+        # ADD THE USER'S GUESS TO THE ARRAY OF PREVIOUS GUESSES:
         mastermind.add_guess(user_guess)
+        # TAKE THE USER'S CURRENT GUESS AND CHECK TO SEE IF ANY ARE CORRECT:
         result = mastermind.check_guess(user_guess)
+        # GIVE RESULT ELEMENTS PROPER VARIABLE NAMES:
         correct_numbers, correct_positions, correct_instances = result[0], result[1], result[2]
+        # ALLOW USERS TO VIEW THEIR GUESSES AND REMAINING GUESSES:
         guesses = mastermind.view_guesses()
         remaining_guesses = mastermind.remaining_guesses()
+        # WRITE SCORING LOGIC:
+        # IF USER GUESSES NO CORRECT NUMBERS:
         if correct_numbers == 0:
             hint_needed = input(Fore.RED + f'Sorry, those are all incorrect. Do you need a hint? Type Yes or No: ' + Fore.RESET)
             mastermind.hint(hint_needed)
+        # IF USER GUESSES SOME CORRECT NUMBERS:
         if correct_positions == 0:
             player_score += correct_instances
+        # IF USER GUESSES A CORRECT NUMBER IN THE RIGHT POSITION:
         else:
             player_score += (correct_positions * 4) + correct_instances
+        # AT EVERY ITERATION/GUESS, ALLOW THE USER TO SEE THEIR SCORES, THE NUMBER OF NUMBERS AND POSITIONS THEY GOT CORRECT, AS WELL AS THEIR REMAINING AND PREVIOUS GUESSES:
         display_guesses(correct_numbers, correct_positions, player_score, remaining_guesses, guesses)
+    # WRITE WINNING LOGIC:
     if mastermind.combination_found:
         print(Fore.GREEN + 'Congratulations! You have guessed the hidden combination correctly.' + Fore.RESET)
     else:
@@ -41,6 +60,7 @@ def get_random_combination():
     if response.status_code == 200:
         combination = ''
         combination += response.text
+        # REPLACES ALL OF THE ROW SPACES WITH JUST ONE SPACE, MAKING THE RETURNED TEXT '1234' FORMAT:
         hidden_combination = combination.replace('\n', '')
         return hidden_combination
     else:
