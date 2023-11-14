0df29e1c2292ecc7a41da01536d180e10f28f20c
gabeantonio <antoniogabe99@gmail.com>
2023-11-13 20:25:56 -0800

    Remove all pseudocode

--- init_mastermind.py
+++ init_mastermind.py
@@ -5,32 +5,21 @@ import math
 import time
 
 def main():
-    # ADD GAME RULES FOR THE PLAYER:
     intro()
-    # ASK USER IF THEY WANT TO INCREASE THE DIFFICULTY OF THE GAME:
     difficulty = input(Fore.RED + 'Do you want to play on a hard difficulty level? Type Yes or No: ' + Fore.RESET)
     if difficulty.lower() == 'yes':
-        # MAKE API CALL AND STORE RETURNED COMBINATION IN THE BELOW VARIABLE:
         hidden_combination = get_random_combination(5)
     else:
-        # MAKE API CALL AND STORE RETURNED COMBINATION IN THE BELOW VARIABLE:
         hidden_combination = get_random_combination(4)
-    # FOR TESTING:
     print('HIDDEN COMBINATION: ', hidden_combination)
-    # INITIALIZE THE PLAYER SCORE TO 0:
     player_score = 0
-    # CALL THE PLAY GAME FUNCTION:
     play_game(hidden_combination, player_score)
 
 def play_game(hidden_combination: str, player_score: int):
-    # INSTANTIATE THE MASTERMIND CLASS:
     mastermind = Mastermind(hidden_combination)
     start = time.time()
-    # INITIALIZE A WHILE LOOP TO CONTINUE LOOPING UNTIL THE MASTERMIND.CONTINUE_GUESSING FUNCTION IS STILL TRUE:
     while mastermind.continue_guessing():
-        # GET THE USER'S GUESS:
         user_guess = input('Type your guess here: ')
-        # WRITE LOGIC FOR USER INPUT VALIDATION:
         if not user_guess.isnumeric():
             print(Fore.RED + f'Please enter numbers only. \n' + Fore.RESET)
             continue
@@ -40,31 +29,19 @@ def play_game(hidden_combination: str, player_score: int):
         elif len(user_guess) > len(hidden_combination):
             print(Fore.RED + f'Input too long. Please make sure your input is only {len(hidden_combination)} long. \n' + Fore.RESET)
             continue
-        
-        
-        # ADD THE USER'S GUESS TO THE ARRAY OF PREVIOUS GUESSES:
         mastermind.add_guess(user_guess)
-        # TAKE THE USER'S CURRENT GUESS AND CHECK TO SEE IF ANY ARE CORRECT:
         result = mastermind.check_guess(user_guess)
-        # GIVE RESULT ELEMENTS PROPER VARIABLE NAMES:
         correct_numbers, correct_positions, correct_instances = result[0], result[1], result[2]
-        # ALLOW USERS TO VIEW THEIR GUESSES AND REMAINING GUESSES:
         guesses = mastermind.view_guesses()
         remaining_guesses = mastermind.remaining_guesses()
-        # WRITE SCORING LOGIC:
-        # IF USER GUESSES NO CORRECT NUMBERS:
         if correct_numbers == 0:
             hint_needed = input(Fore.RED + f'Sorry, those are all incorrect. Do you need a hint? Type Yes or No: ' + Fore.RESET)
             mastermind.hint(hint_needed)
-        # IF USER GUESSES SOME CORRECT NUMBERS:
         if correct_positions == 0:
             player_score += correct_instances
-        # IF USER GUESSES A CORRECT NUMBER IN THE RIGHT POSITION:
         else:
             player_score += (correct_positions * 4) + correct_instances
-        # AT EVERY ITERATION/GUESS, ALLOW THE USER TO SEE THEIR SCORES, THE NUMBER OF NUMBERS AND POSITIONS THEY GOT CORRECT, AS WELL AS THEIR REMAINING AND PREVIOUS GUESSES:
-        display_guesses(correct_numbers, correct_positions, player_score, remaining_guesses, guesses)
-    # WRITE WINNING LOGIC:
+        display_stats(correct_numbers, correct_positions, player_score, remaining_guesses, guesses)
     if mastermind.combination_found:
         end = time.time()
         print(Fore.GREEN + f'Congratulations! You have guessed the hidden combination correctly. You guessed it in {math.floor(end - start)} seconds. Can you guess it faster next time?' + Fore.RESET)
@@ -75,13 +52,12 @@ def get_random_combination(difficulty: int):
     api_url = f"https://www.random.org/integers/?num={difficulty}&min=0&max=7&col=1&base=10&format=plain&rnd=new"
     response = requests.get(api_url)
     if response.status_code == 200:
-        # REPLACES ALL OF THE ROW SPACES WITH JUST ONE SPACE, MAKING THE RETURNED TEXT '1234' FORMAT:
         hidden_combination = f'{response.text}'.replace('\n', '')
         return hidden_combination
     else:
         print(f'Failed to get a random combination. Status code: {response.status_code}')
 
-def display_guesses(correct_numbers: int, correct_positions: int, player_score: int, remaining_guesses: int, guesses: list):
+def display_stats(correct_numbers: int, correct_positions: int, player_score: int, remaining_guesses: int, guesses: list):
     print(Fore.GREEN + f'You have guessed {correct_numbers} correct numbers and {correct_positions} correct positions.' + Fore.RESET)
     print(Fore.GREEN + f'Your score: {player_score}' + Fore.RESET)
     print(Fore.YELLOW + f'You have {remaining_guesses} remaining guesses.' + Fore.YELLOW)
--- mastermind.py
+++ mastermind.py
@@ -7,8 +7,6 @@ class Mastermind:
         self.hidden_combination = hidden_combination
         self.guesses = []
 
-    # Write winning logic:
-    # Make it a property so it doesnt get called before you can input anything.
     @property
     def combination_found(self):
         if len(self.guesses) > 0 and self.guesses[-1] == self.hidden_combination:
@@ -16,15 +14,12 @@ class Mastermind:
         else: 
             return False
 
-    # Write logic in the event that a user attempts a combination:
     def add_guess(self, combination: str):
         self.guesses.append(combination)
 
-    # Write logic for a user to be able to view their past guesses:
     def view_guesses(self):
         return self.guesses
     
-    # Write logic that checks if the guess is correct, partially correct, or incorrect:
     def check_guess(self, user_guess: str):
         in_combination = []
         correct_positions = 0
@@ -36,19 +31,16 @@ class Mastermind:
                 correct_positions += 1
         correct_numbers = len(set(in_combination))
         return [correct_numbers, correct_positions, len(in_combination)]
-        
-    # Write logic so that the user can view how many guesses they have left:
+
     def remaining_guesses(self):
         return self.TOTAL_TRIES - len(self.guesses)
 
-    # Write logic that checks if a user can still make guesses:
     def continue_guessing(self):
         if self.TOTAL_TRIES - len(self.guesses) > 0 and self.combination_found == False:
             return True
         else: 
             return False
         
-    # Write logic so that the user can get a hint if they need it:
     def hint(self, hint_needed: str):
         if hint_needed.lower() == 'yes':
             random_index = random.randint(0, len(self.hidden_combination) - 1)
